7980bf65eebbca5d3089c68e20f4f1ae2b06c38e
Arindam Mishra <arindam5191@gmail.com>
2015-04-25 10:45:12 +0530

    Update ideone_pointer_del.c

--- ideone_pointer_del.c
+++ ideone_pointer_del.c
@@ -12,7 +12,22 @@ struct node
 /* Given a reference (pointer to pointer) to the head
     of a list and an int, push a new node on the front
     of the list. */
-
+void push(struct node** head_ref, int new_data)
+{
+   /* allocate node */
+   struct node* new_node =
+             (struct node*) malloc(sizeof(struct node));
+ 
+   /* put in the data  */
+   new_node->data  = new_data;
+ 
+   /* link the old list off the new node */
+   new_node->next = (*head_ref);
+ 
+   /* move the head to point to the new node */
+   (*head_ref)    = new_node;
+}
+ 
 void printList(struct node *head)
 {
    struct node *temp = head;
