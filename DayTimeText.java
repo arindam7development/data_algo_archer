383c01816271c9a81e462a6a30d25da5057b3fa4
arindam7development <arindam5191@gmail.com>
2015-07-12 10:07:43 +0530

    back on git

---
+++ DayTimeText.java
@@ -0,0 +1,28 @@
+package code_practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class DayTimeText {
+
+	public static void main(String[] argv) {
+		final short TIME_PORT = 80;
+		String hostName;
+		if (argv.length == 0)
+		hostName = "localhost";
+		else
+		hostName = argv[0];
+		try {
+		Socket sock = new Socket(hostName, TIME_PORT);
+		BufferedReader is = new BufferedReader(new
+		InputStreamReader(sock.getInputStream( )));
+		String remoteTime = is.readLine( );
+		System.out.println("Time on " + hostName + " is " + remoteTime);
+		} catch (IOException e) {
+		System.err.println(e);
+		}
+	}
+
+}
