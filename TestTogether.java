d2bb3b6913022af555ce59c7254f4c1d2025a69b
Arindam Mishra <arindam5191@gmail.com>
2015-07-16 12:02:02 +0530

    Update TestTogether.java

--- TestTogether.java
+++ TestTogether.java
@@ -1,10 +1,9 @@
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
- * Created by Arindam on 7/16/2015.
+ * Created by AR277544 on 7/16/2015.
  */
 public class TestTogether {
 
@@ -13,6 +12,8 @@ public class TestTogether {
 
         List<String> elephantList = Arrays.asList(url_input.split(","));
         String temp = null;
+        HashMap<String,Integer> hmap = new HashMap<String,Integer>();
+
         for (int i=0;i<elephantList.size();i++){
             temp = elephantList.get(i);
             try {
@@ -23,14 +24,24 @@ public class TestTogether {
                 int code = connection.getResponseCode();
                 String res_msg = connection.getResponseMessage();
                 System.out.println(temp+" "+"says"+" "+res_msg+" "+code+" "+"Success");
-
+                hmap.put(temp,code);
             } catch (Exception e) {
 
             }
         }
 
+        Iterator it = hmap.entrySet().iterator();
+        while (it.hasNext()) {
+
+            Map.Entry pair = (Map.Entry)it.next();
+
+            System.out.println("Hi I am hash map ."+" "+"I have "+pair.getKey()+" "+pair.getValue());
+        }
 
 
-    }
+
+
+
+        }
 
 }
