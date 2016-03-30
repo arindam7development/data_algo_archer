5786a4fa4ebeaa8a143f43e131a0cb8014bda6f4
arindam7development <arindam5191@gmail.com>
2016-03-30 06:33:26 +0530

    Generics

---
+++ src/ListNode.java
@@ -0,0 +1,16 @@
+/**
+ * Created by AR277544 on 3/29/2016.
+ */
+public class ListNode<E> {
+
+    ListNode<E> next ;
+    E data ;
+
+    ListNode(E p){
+        next = null;
+        this.data=p ;
+
+    }
+
+}
+
