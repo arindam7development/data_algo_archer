1d9470a8c9ebf70890bbd8d65a4be2324997feb3
arindam7development <arindam5191@gmail.com>
2016-03-30 06:33:58 +0530

    Generics

---
+++ src/MyLinkedList.java
@@ -0,0 +1,36 @@
+/**
+ * Created by AR277544 on 3/29/2016.
+ */
+public class MyLinkedList<T>{
+
+
+
+public static void main(String [] args){
+
+    Return rString = new Return("arindam ");
+
+    Return rInt = new Return(123);
+
+   // rString.getData();
+
+
+ rString.show(rString.getData());
+    rInt.show(rInt.getData());
+
+    Integer i= (Integer)rString.arin(new Integer(777)) ;
+
+    String s = (String) rInt.arin("ARINDAM");
+
+    rString.show(i);
+    rInt.show(s);
+
+
+
+
+}
+
+
+
+
+
+}
